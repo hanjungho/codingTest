433c09c44e360395e22a75efb6f8437fc933508c
Jungho Han <thegamerhan@gmail.com>
2025-01-27 18:35:49 +0900

    [level 2] Title: 오픈채팅방, Time: 249.31 ms, Memory: 102 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,23 @@
+function solution(record) {
+    const obj = new Map();
+    const msg = [];
+    for (const i of record) {
+        const a = i.split(' ');
+        if (a[0] === "Enter") {
+            obj.set(a[1], a[2]);
+        } else if (a[0] === "Change") {
+            obj.set(a[1], a[2]);
+        }
+    }
+    
+    for (const i of record) {
+        const a = i.split(' ');
+        if (a[0] === "Enter") {
+            msg.push(`${obj.get(a[1])}님이 들어왔습니다.`);
+        } else if (a[0] === "Leave") {
+            msg.push(`${obj.get(a[1])}님이 나갔습니다.`);
+        }
+    }
+    
+    return msg;
+}
