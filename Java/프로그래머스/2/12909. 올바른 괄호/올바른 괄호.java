7f043c9fc60270094bd8468ac9e890cc37524f67
Jungho Han <thegamerhan@gmail.com>
2025-02-25 15:07:22 +0900

    [level 2] Title: 올바른 괄호, Time: 15.55 ms, Memory: 54.8 MB -BaekjoonHub

--- Java/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ Java/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,24 +4,17 @@ class Solution {
     boolean solution(String s) {
         Deque<Character> deque = new ArrayDeque<>();
         
-        boolean answer = true;
-        
         for (char c : s.toCharArray()) {
             if (c == '(') {
                 deque.push(c);
             } else {
                 if (deque.isEmpty()) {
-                    answer = false;
-                    break;
+                    return false;
                 }
                 deque.pop();
             }
         }
-        
-        if (deque.isEmpty() == false) {
-            answer = false;
-        }
 
-        return answer;
+        return deque.isEmpty();
     }
 }
