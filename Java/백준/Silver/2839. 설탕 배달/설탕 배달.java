5bf4f5df781b8db47da7e754cde374e4556d7928
Jungho Han <thegamerhan@gmail.com>
2025-08-27 03:11:26 +0900

    [Silver IV] Title: 설탕 배달, Time: 112 ms, Memory: 14300 KB -BaekjoonHub

---
+++ Java/백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    static int n;
+    
+    static int[] bags = {3, 5};
+    
+    public static void main(String[] args) throws IOException {
+        
+        input();
+        output(solve());
+    }
+    
+    public static void input() throws IOException {
+        
+        n = Integer.parseInt(br.readLine());
+        
+        br.close();
+    }
+    
+    public static String solve() {
+        
+        int maxNumOfMaxWeightBags = n / bags[1];
+        
+        for (int numOfMaxWeightBags = maxNumOfMaxWeightBags; numOfMaxWeightBags >= 0; numOfMaxWeightBags--) {
+            
+            int minNumOfBags = numOfMaxWeightBags;
+            
+            if ((n - (numOfMaxWeightBags * bags[1])) % bags[0] == 0) {
+                
+                minNumOfBags += (n - (numOfMaxWeightBags * bags[1])) / bags[0];
+                
+                return String.valueOf(minNumOfBags);
+            }
+        }
+        
+        return "-1";
+    }
+    
+    public static void output(String str) throws IOException {
+        
+        bw.write(str);
+        
+        bw.flush();
+        bw.close();
+    }
+}
