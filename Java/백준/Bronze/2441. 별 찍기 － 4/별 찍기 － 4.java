04f200e31f6c05266a603edbcba416422d6e6601
Jungho Han <thegamerhan@gmail.com>
2025-10-14 23:52:34 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 108 ms, Memory: 14376 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    
+    static int n;
+    
+    public static void main(String[] args) throws IOException {
+        
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            
+            input(br);
+            output(bw, solve());
+        }
+    }
+    
+    public static void input(BufferedReader br) throws IOException {
+        
+        n = Integer.parseInt(br.readLine());
+    }
+    
+    public static String solve() {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < n; i++) {
+            
+            for (int j = 0; j < i; j++) {
+                
+                sb.append(" ");
+            }
+            
+            for (int j = 0; j < n - i; j++) {
+                
+                sb.append("*");
+            }
+            
+            sb.append("\n");
+        }
+        
+        return sb.toString();
+    }
+    
+    public static void output(BufferedWriter bw, String outputStr) throws IOException {
+        
+        bw.write(outputStr);
+        
+        bw.flush();
+    }
+}
