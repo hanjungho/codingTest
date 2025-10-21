2eed1e3d555fa77d150842bd85d75b771f33655b
Jungho Han <thegamerhan@gmail.com>
2025-10-21 23:58:50 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 92 ms, Memory: 13876 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    
+    public static void main(String[] args) throws IOException {
+        
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            
+            bw.write("고려대학교");
+            bw.flush();
+        }
+    }
+}
