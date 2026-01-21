33c48e1b30af463f6a89f3cd43f07a83d048cdf5
Jungho Han <thegamerhan@gmail.com>
2026-01-22 00:26:02 +0900

    [Bronze V] Title: Hook, Time: 92 ms, Memory: 13996 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            
+            input(br);
+            output(bw, solve());
+        }
+    }
+    
+    public static void input(BufferedReader br) throws IOException {
+        
+        
+    }
+    
+    public static String solve() {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("#  # #### #### #  #\n#### #  # #  # # #\n#### #  # #  # # #\n#  # #### #### #  #");
+        
+        return sb.toString();
+    }
+    
+    public static void output(BufferedWriter bw, String outputStr) throws IOException {
+        
+        bw.write(outputStr);
+        
+        bw.flush();
+        bw.close();
+    }
+}
